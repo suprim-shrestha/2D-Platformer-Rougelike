f2f03b8bcb303706c2c78fbeee79bb5be6709fce
Suprim Shrestha <suprim.shresth@gmail.com>
2023-12-08 10:51:02 +0545

    Display player on screen

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,22 @@
 const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth * 0.9;
+canvas.height = window.innerHeight * 0.9;
+
+const player = new PlayerInstance(canvas.width / 2, canvas.height - 100);
+
+// Set maximum framerate for higher refresh rate screens
+let lastRenderTime = 0;
+let frameDuration = 1000 / FRAME_RATE;
+function animate(currentTime) {
+  const timeSinceLastRender = currentTime - lastRenderTime;
+  if (timeSinceLastRender >= frameDuration) {
+    lastRenderTime = currentTime - (timeSinceLastRender % frameDuration);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    player.update();
+  }
+  requestAnimationFrame(animate);
+}
+
+animate();
