6bb9122ac61cf0283b3475362b45024915047c62
Suprim Shrestha <suprim.shresth@gmail.com>
2023-12-08 10:50:33 +0545

    Create character class for any moving character

---
+++ scripts/Class/CharacterInstance.js
@@ -0,0 +1,48 @@
+/**
+ * Used for instances of characters
+ *
+ * stats {maxhp, attack, hpRegen, armor, speed}
+ * facingDirection
+ * isBoss
+ * hp
+ *
+ * setInitialStats()
+ * levelUpStats()
+ * fireBullet()
+ * kill()
+ * applyBuff()
+ * removeBuff()
+ * hasBuff()
+ * getBuffs()
+ * moveTo()
+ */
+
+class CharacterInstance extends Instance {
+  constructor(x, y, width = 32, height = 32, sprite) {
+    super(x, y, width, height, sprite);
+    this.vx = 0;
+    this.vy = 0;
+    this.isGrounded = false;
+    this.facingLeft = 0;
+    this.movementDisabled = false;
+  }
+
+  update() {
+    if (!this.movementDisabled) {
+      this.x += this.vx;
+    }
+    this.draw();
+    this.applyGravity();
+  }
+
+  applyGravity() {
+    this.y += this.vy;
+    if (this.y + this.height >= canvas.height) {
+      this.vy = 0;
+      this.y = canvas.height - this.height;
+      this.isGrounded = true;
+    } else {
+      this.vy += GRAVITY;
+    }
+  }
+}
