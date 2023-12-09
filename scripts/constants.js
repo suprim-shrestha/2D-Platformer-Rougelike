8010855be30194d19aefa509125679120a846fdc
Suprim Shrestha <suprim.shresth@gmail.com>
2023-12-09 18:37:36 +0545

    Add scale

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,10 +1,12 @@
-const JUMP_HEIGHT = 5;
+const JUMP_HEIGHT = 3;
 
-const GRAVITY = 0.4;
+const GRAVITY = 0.25;
 
-const SPEED = 4;
+const SPEED = 2;
 
 const FRAME_RATE = 60;
 
 const FACING_LEFT = -1;
 const FACING_RIGHT = 1;
+
+const SCALE = 2;
