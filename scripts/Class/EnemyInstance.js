86446dc0a91b45ffc336de12a56ffbc095f8399a
Suprim Shrestha <suprim.shresth@gmail.com>
2023-12-15 20:52:43 +0545

    Kill enemy if it falls out of map

--- scripts/Class/EnemyInstance.js
+++ scripts/Class/EnemyInstance.js
@@ -69,6 +69,9 @@ class EnemyInstance extends CharacterInstance {
       this.jump();
     }
     this.checkSpecialBlockCollision();
+    if (this.y > MAP_WIDTH) {
+      this.kill();
+    }
   }
 
   jump() {
